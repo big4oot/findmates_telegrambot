d51e7cc489f0bcb44b37f9b40c73767698aabad1
bigfoot <bigfoot@com>
2021-05-06 14:48:52 +0300

    add bot_config for heroku

---
+++ source/bot_config.py
@@ -0,0 +1,9 @@
+token = '1701261585:AAFnbXsIxbjD6_ebYbQJvvrTDOGW5dh8HCU'
+
+url_finders = "https://usersprojectsdb-ef61.restdb.io/rest/finders"
+url_projects = "https://usersprojectsdb-ef61.restdb.io/rest/projects"
+headers = {
+    'content-type': "application/json",
+    'x-apikey': "de9b868e6422e6df25a035f9592b118ed8a85",
+    'cache-control': "no-cache"
+    }
