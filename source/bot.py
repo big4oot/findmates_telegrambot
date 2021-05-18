4508add58b20249f08b830d71fd209ef36bdf6fb
bigfoot <bigfoot@com>
2021-05-18 12:02:12 +0300

    added callback buttons instead commands added edit user profile feature

--- source/bot.py
+++ source/bot.py
@@ -1,23 +1,20 @@
+import random
+import json
 import telebot
 import bot_config
 import db
-import json
 import time
-import random
+import keyboard
+
 
 '''
 TODO:
 markdown
 upload CV
-fix some bugs
 validation userinput during registration
-buttons instead commands
 choosing post(position) for user (like frontend dev, data scientist etc.)
 edit profile
 check user (exist?) before registration
-help with commands
-get users profiles
-
 change fun name???
 '''
 
@@ -25,216 +22,307 @@ bot = telebot.TeleBot(bot_config.token)
 
 
 class User():
-	def __init__(self):
-		self.name = None
-		self.id = None
-		self.tgname = None
-		self.skills = []
-		self.info = ''
-		self.isActive = True
+    def __init__(self):
+        self.name = None
+        self.id = None
+        self.tgname = None
+        self.skills = []
+        self.info = ''
+        self.isActive = True
 
-	def payload_to_db(self):
-		payload = json.dumps( {"user_name": self.name,
-				"user_id": self.id,
-				"user_tgname": self.tgname,
-				"user_skills":self.skills,
-				"users_info": self.info,
-				"user_isActive": self.isActive
-			} 
-		)
-		db.Users_db.payload_data(payload)
-		bot.send_message(self.id, "Регистрация завершена")
+    def payload_to_db(self):
+        payload = json.dumps({"user_name": self.name,
+                              "user_id": self.id,
+                              "user_tgname": self.tgname,
+                              "user_skills": self.skills,
+                              "users_info": self.info,
+                              "user_isActive": self.isActive})
+        db.Users_db.payload_data(payload)
+        bot.send_message(self.id, "Регистрация завершена")
+        show_main_kboard(self.id)
 
 
 class Project():
-	def __init__(self):
-		self.id = None
-		self.owner_username = None
-		self.owner_id = None
-		self.title = None
-		self.description = None
-		self.required_roles = {}
-		self.isActive = True
+    def __init__(self):
+        self.id = None
+        self.owner_username = None
+        self.owner_id = None
+        self.title = None
+        self.description = None
+        self.required_roles = {}
+        self.isActive = True
 
-	def payload_to_db(self):
-		payload = json.dumps( {"user_name": self.title,
-				"project_id": self.id,
-				"project_owner_username": self.owner_username,
-				"project_title":self.title,
-				"project_description": self.description,
-				"project_required_roles": self.required_roles,
-				"project_isActive": self.isActive
-			} 
-		)
-		db.payload_data(payload, 'p')
-		bot.send_message(self.owner_id, "Регистрация завершена")
+    def payload_to_db(self):
+        payload = json.dumps({"user_name": self.title,
+                              "project_id": self.id, 	
+                              "project_owner_username": self.owner_username,
+                              "project_title" : self.title,
+                              "project_description": self.description,
+                              "project_required_roles": self.required_roles,
+                              "project_isActive": self.isActive})
+        db.Projects_db.payload_data(payload)
+        bot.send_message(self.owner_id, "Регистрация завершена")
+        show_main_kboard(owner_id)
 
 
 user = None
 project = None
+user_data = None
 
+
+def show_main_kboard(user_id):
+	bot.send_message(user_id, text="Выберите действие", reply_markup=keyboard.main_kboard())
+
+
+def edit_user(call):
+    global user_data
+    user_id = str(call.from_user.id)
+    user_data = db.Users_db.get_user_info(user_id)
+    if user_data == None:
+        bot.send_message(user_id, text="Пользователь не найден")
+    else:
+        text = f"""Ваш профиль:
+            Имя: {user_data["user_name"]}
+            Telegram-ник: {user_data["user_tgname"]}
+            Навыки: {user_data["user_skills"]}
+            Доп. информация: {user_data["users_info"]}
+            Анкета активна: {'Да' if user_data["user_isActive"] else 'Нет'}
+            ____________________________
+            Выберите поле для изменения
+            """
+        bot.send_message(user_id, text, reply_markup=keyboard.edit_choice_kboard())
+
+
+def edit_name(call):
+    msg = bot.send_message(call.from_user.id, "Введите новое имя")
+    bot.register_next_step_handler(msg, edit_field_name)
+    
+
+def edit_field_name(message):
+    db.Users_db.edit_field("user_name", user_data["_id"], message.text)
+    bot.send_message(message.from_user.id, "Информация обновлена")
+    show_main_kboard(message.from_user.id)
+
+def edit_skills(call):
+    text = "Укажите через запятую свои навыки, например: \n" \
+           "Python, Django, HTML, CSS, SQL, Git"
+    msg = bot.send_message(call.from_user.id, text)
+    bot.register_next_step_handler(msg, edit_field_skills)
+
+
+def edit_field_skills(message):
+    skills = list(map(str.lower, message.text.replace(' ', '').split(',')))
+    db.Users_db.edit_field("user_skills", user_data["_id"], skills)
+    bot.send_message(message.from_user.id, "Информация обновлена")
+    show_main_kboard(message.from_user.id)
+
+
+def edit_info(call):
+    msg = bot.send_message(call.from_user.id, "Введите новую информацию о себе")
+    bot.register_next_step_handler(msg, edit_field_info)
+
+
+def edit_field_info(message):
+    db.Users_db.edit_field("users_info", user_data["_id"], message.text)
+    bot.send_message(message.from_user.id, "Информация обновлена")
+    show_main_kboard(message.from_user.id)
+
+
+def edit_active(call):
+    db.Users_db.edit_field("user_isActive", user_data["_id"], not(user_data["user_isActive"]))
+    bot.send_message(call.from_user.id, "Информация обновлена")
+    show_main_kboard(call.from_user.id)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.data == 'reg_user':
+       reg_dev(call)
+    elif call.data == 'reg_proj':
+       reg_proj(call)
+    elif call.data == 'show_users':
+       show_users(call)
+    elif call.data == 'show_projs':
+       show_projects(call)
+    elif call.data == 'find_users':
+       pass
+    elif call.data == 'find_proj':
+       pass
+    elif call.data == 'accept_u':
+       accept('u')
+    elif call.data == 'accept_p':
+       accept('p')
+    elif call.data == 'edit_user':
+       edit_user(call)
+    elif call.data == 'edit_name':
+        edit_name(call)
+    elif call.data == 'edit_skills':
+        edit_skills(call)
+    elif call.data == 'edit_info':
+        edit_info(call)
+    elif call.data == 'edit_active':
+        edit_active(call)
 
 @bot.message_handler(commands=['show_users'])
 def show_users(message):
-	bot.send_message(message.from_user.id, db.Users_db.get_users_page())
+    bot.send_message(message.from_user.id, db.Users_db.get_users_page())
+    show_main_kboard(message.from_user.id)
 
 
 @bot.message_handler(commands=['show_projects'])
-def show_users(message):
-	bot.send_message(message.from_user.id, db.Projects_db.get_projects_page())
+def show_projects(message):
+    bot.send_message(message.from_user.id, db.Projects_db.get_projects_page())
+    show_main_kboard(message.from_user.id)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-	text = '''
-		Привет. Ты разработчик, который ищет возможность поучаствовать в командной разработке?
-		Или же ты руководитель проекта, который нуждается в разработчиках?
-		Этот бот окажет содействие в кооперации разработчиков и проектных команд.
-		Для начала зарегистрируйся:
-			# /reg_dev - как разработчик
-			# /reg_proj - как руководитель проектной команды
-	'''
-	bot.send_message(message.from_user.id, text)
+    text = "Привет!\nТы разработчик, который ищет возможность поучаствовать" \
+           "в командной разработке?\nИли же ты руководитель проекта, " \
+           "который нуждается в разработчиках?\nЭтот бот окажет содействие" \
+           " в кооперации разработчиков и проектных команд."
+    bot.send_message(message.from_user.id, text, parse_mode="HTML")
+    show_main_kboard(message.from_user.id)
 
 
 @bot.message_handler(commands=['reg_dev'])
 def reg_dev(message):
-	if message.from_user.username is None:
-		text = '''Внимание. У Вас не установлено имя пользователя. Без него с Вами не смогут связаться другие люди.
-		Для продолжения, пожалуйста, установите имя пользователя (username) в настройках Telegram, после чего вызовите команду для регистрации снова.
-		'''
-		bot.send_message(message.from_user.id, text)
-		return False
+    if message.from_user.username is None:
+        text = "Внимание. У Вас не установлено имя пользователя. Без него" \
+               " с Вами не смогут связаться другие люди. " \
+               "Для продолжения, пожалуйста, установите имя пользователя" \
+               "(username) в настройках Telegram, " \
+               "после чего вызовите команду для регистрации снова."
+        bot.send_message(message.from_user.id, text)
+        return False
 
-	global user
-	text = '''
-		Предупреждение. Сведения, указанные вами при регистрации будут доступны другим
-		участникам бота. Но вы всегда можете приостановить отображение своей анкеты (а также 
-		отредактировть другие данные) при помощи команды /profile_edit
-		'''
-	bot.send_message(message.from_user.id, text)
-	time.sleep(2)
-	user = User()
-	user.id = message.from_user.id
-	user.tgname = "@" + message.from_user.username
+    global user
+    text = "Предупреждение. Сведения, указанные вами при регистрации будут" \
+           "доступны другим участникам бота. Но вы всегда можете" \
+           "приостановить отображение своей анкеты (а также " \
+           "отредактировть другие данные) при помощи команды /profile_edit"
+    bot.send_message(message.from_user.id, text)
+    time.sleep(2)
+    user = User()
+    user.id = message.from_user.id
+    user.tgname = "@" + message.from_user.username
 
-	msg = bot.send_message(message.from_user.id, "Введите Ваше имя")
-	bot.register_next_step_handler(msg, reg_dev_name_step)
+    msg = bot.send_message(message.from_user.id, "Введите Ваше имя")
+    bot.register_next_step_handler(msg, reg_dev_name_step)
 
 
 @bot.message_handler(commands=['reg_proj'])
 def reg_proj(message):
-	if message.from_user.username is None:
-		text = '''Внимание. У Вас не установлено имя пользователя. Без него с Вами не смогут связаться другие люди.
-		Для продолжения, пожалуйста, установите имя пользователя (username) в настройках Telegram, после чего вызовите команду для регистрации снова.
-		'''
-		bot.send_message(message.from_user.id, text)
-		return False
+    if message.from_user.username is None:
+        text = "Внимание. У Вас не установлено имя пользователя. Без него" \
+               " с Вами не смогут связаться другие люди. " \
+               "Для продолжения, пожалуйста, установите имя пользователя" \
+               "(username) в настройках Telegram, " \
+               "после чего вызовите команду для регистрации снова."
+        bot.send_message(message.from_user.id, text)
+        return False
 
-	global project
-	text = '''
-		Предупреждение. Сведения, указанные вами при регистрации будут доступны другим
-		участникам бота. Но вы всегда можете приостановить отображение своего проекта (а также 
-		отредактировть другие данные) при помощи команды /project_edit
-		'''
-	bot.send_message(message.from_user.id, text)
-	time.sleep(2)
-	project = Project()
-	project.owner_id = message.from_user.id
-	project.id = random.randint(100000,999999)
-	project.owner_username = "@" + message.from_user.username
+    global project
+    text = "Предупреждение. Сведения, указанные вами при регистрации будут" \
+           "доступны другим участникам бота. Но вы всегда можете" \
+           "приостановить отображение своего проекта (а также " \
+           "отредактировть другие данные) при помощи команды /project_edit"
+    bot.send_message(message.from_user.id, text)
+    time.sleep(2)
+    project = Project()
+    project.owner_id = message.from_user.id
+    project.id = random.randint(100000,999999)
+    project.owner_username = "@" + message.from_user.username
 
-	msg = bot.send_message(message.from_user.id, "Введите название проекта")
-	bot.register_next_step_handler(msg, reg_proj_title_step)
+    msg = bot.send_message(message.from_user.id, "Введите название проекта")
+    bot.register_next_step_handler(msg, reg_proj_title_step)
 
 
 def reg_proj_title_step(message):
-	global project
-	title = message.text
-	project.title = title
-	text = "Опишите Ваш проект"
-	msg = bot.send_message(message.from_user.id, text)
-	bot.register_next_step_handler(msg, reg_proj_descr_step)
+    global project
+    title = message.text
+    project.title = title
+    text = "Опишите Ваш проект"
+    msg = bot.send_message(message.from_user.id, text)
+    bot.register_next_step_handler(msg, reg_proj_descr_step)
 
 
 def reg_proj_descr_step(message):
-	global project
-	description = message.text
-	project.description = description
-	text = """ Укажите (через запятую) требуемые в команду позиции. 
-		Например, если вам требуется два frontend-разработчка и один QA, то следует отправить
-		боту текст: frontend, frontend, QA
-	"""
-	msg = bot.send_message(message.from_user.id, text)
-	bot.register_next_step_handler(msg, reg_proj_roles_step)
+    global project
+    description = message.text
+    project.description = description
+    text = "Укажите (через запятую) требуемые в команду позиции. \n" \
+           "Например, если вам требуется два frontend-разработчка " \
+           "и один QA, то следует отправить боту текст: frontend, frontend, QA"
+    msg = bot.send_message(message.from_user.id, text)
+    bot.register_next_step_handler(msg, reg_proj_roles_step)
 
 
 def reg_proj_roles_step(message):
-	global project
-	roles = list(map(str.lower,message.text.replace(' ', '').split(',')))
-	project.required_roles = dict.fromkeys(roles, False)
-	text = f'''Проверьте введенные данные:
-			   Ваш телеграм-никнейм: {project.owner_username}
-			   Название проекта: {project.title}
-			   Описание проекта: {project.description}
-			   Требуемые роли в проекте: {roles}
-			   Проект активен: {'Да' if project.isActive else 'Нет'}
-			   ____________________________
-			   Подтвердите регистрацию командой /accept 2
-			'''
-	bot.send_message(message.from_user.id, text)
+    global project
+    roles = list(map(str.lower,message.text.replace(' ', '').split(',')))
+    project.required_roles = dict.fromkeys(roles, False)
+    text = f'''Проверьте введенные данные:
+               Ваш телеграм-никнейм: {project.owner_username}
+               Название проекта: {project.title}
+               Описание проекта: {project.description}
+               Требуемые роли в проекте: {roles}
+               Проект активен: {'Да' if project.isActive else 'Нет'}
+               ____________________________
+               Подтвердите регистрацию
+            '''
+    bot.send_message(message.from_user.id, text, reply_markup=keyboard.accept_kboard('proj'))
 
 
 @bot.message_handler(commands=['accept'])
 def accept(message):
-	'''
-	accept types:
-	1 - accept developer registration
-	2 - accept project registration 
-	'''
-	accept_type = message.text.replace(' ','')[-1]
-	if accept_type == '1':
-		user.payload_to_db()
-	elif accept_type == '2':
-		project.payload_to_db()
+    '''
+    accept types:
+    u - accept developer registration
+    p - accept project registration
+    '''
+    accept_type = message
+    if accept_type == 'u':
+        user.payload_to_db()
+    elif accept_type == 'p':
+        project.payload_to_db()
 
 
 def reg_dev_name_step(message):
-	global User
-	name = message.text
-	print(name)
-	user.name = name
-	text = '''Укажите через запятую свои навыки, например:
-		Python, Django, HTML, CSS, SQL, Git'''
-	msg = bot.send_message(message.from_user.id, text)
-	bot.register_next_step_handler(msg, reg_dev_skills_step)
+    global User
+    name = message.text
+    print(name)
+    user.name = name
+    text = "Укажите через запятую свои навыки, например: \n" \
+           "Python, Django, HTML, CSS, SQL, Git"
+    msg = bot.send_message(message.from_user.id, text)
+    bot.register_next_step_handler(msg, reg_dev_skills_step)
 
 
 def reg_dev_skills_step(message):
-	global user
-	skills = list(map(str.lower,message.text.replace(' ', '').split(',')))
-	user.skills = skills
-	text = '''
-		Далее добавьте к своей анкете сопроводительный текст. В нем можно указать, 
-		например, опыт работы или иную информацию, которую считаете нужной и уместной.
-		'''
-	msg = bot.send_message(message.from_user.id, text)
-	bot.register_next_step_handler(msg, reg_dev_info_step)
+    global user
+    skills = list(map(str.lower, message.text.replace(' ', '').split(',')))
+    user.skills = skills
+    text = "Далее добавьте к своей анкете сопроводительный текст. " \
+           "В нем можно указать, например, опыт работы или иную информацию, " \
+           "которую считаете нужной и уместной."
+    msg = bot.send_message(message.from_user.id, text)
+    bot.register_next_step_handler(msg, reg_dev_info_step)
 
 
 def reg_dev_info_step(message):
-	global user
-	info = message.text
-	user.info = info
-	text = f'''Проверьте введенные данные:
-			   Имя: {user.name}
-			   Telegram-ник: {user.tgname}
-			   Навыки: {user.skills}
-			   Доп. информация: {user.info}
-			   Анкета активна: {'Да' if user.isActive else 'Нет'}
-			   ____________________________
-			   Подтвердите регистрацию командой /accept 1
-			'''
-	bot.send_message(message.from_user.id, text)
+    global user
+    info = message.text
+    user.info = info
+    text = f"""Проверьте введенные данные:
+               Имя: {user.name}
+               Telegram-ник: {user.tgname}
+               Навыки: {user.skills}
+               Доп. информация: {user.info}
+               Анкета активна: {'Да' if user.isActive else 'Нет'}
+               ____________________________
+               Подтвердите регистрацию
+            """
+    bot.send_message(message.from_user.id, text, reply_markup=keyboard.accept_kboard("user"))
+
 
 bot.polling(none_stop=True, interval=0)
--- source/db.py
+++ source/db.py
@@ -20,6 +20,27 @@ class Users_db():
 						f"Навыки: {', '.join(user['user_skills'])} \n" \
 						f"Доп. информация: {user['users_info']} \n\n" 		
 		return text
+	
+	
+	def get_user_info(user_id):
+		db_user_id = None
+		response = requests.request("GET", Users_db._url, headers=bot_config.headers)
+		response = json.loads(response.text)
+		for user in response:
+		  if user["user_id"] == (user_id):
+			  return user
+	
+	
+	def edit_field(field, db_id, new_data):
+		url = f"https://usersprojectsdb-ef61.restdb.io/rest/finders/{db_id}"
+		payload = json.dumps({f"{field}" : new_data})
+		headers = {
+    		'content-type': "application/json",
+    		'x-apikey': "de9b868e6422e6df25a035f9592b118ed8a85",
+    		'cache-control': "no-cache"
+		}
+		response = requests.request("PUT", url, data=payload, headers=bot_config.headers)
+		
 
 
 class Projects_db():
